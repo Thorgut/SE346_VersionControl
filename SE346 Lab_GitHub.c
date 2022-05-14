0efdbb9b5e91e7414ab09aa49106192d7cb49b74
Thorgut <tozfidaner@gmail.com>
2022-05-14 08:23:31 +0300

    v1.2

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -6,8 +6,8 @@ int main()
 	
 	//please write your name,student id and department by using printf() function
 
-	printf("Z.Yaren Oguz,123456,SE\n");
+	printf("Turgut Özfidaner,17243610025,ISE\n");
 
-	printf("Ozge Tekin,987654,SE\n");
+	printf("Turgut Özfidaner,17243610025,ISE\n");
 
 }
